c7ceb5b3d56a08dac5cf822ccf022173ed7953d6
KevinU20221275 <U20221275@univo.edu.sv>
2024-02-29 12:20:18 -0600

    method to add consoles to the database was created

--- GameStoreDemo/GameStore/Controllers/ConsoleController.cs
+++ GameStoreDemo/GameStore/Controllers/ConsoleController.cs
@@ -1,13 +1,42 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using GameStore.Data;
+using GameStore.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace GameStore.Controllers
 {
     public class ConsoleController : Controller
     {
+        private readonly AppDbContext _appDbContext;
+
+        public ConsoleController(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+
         // GET: ConsoleController
         public IActionResult Index()
         {
+            IEnumerable<MConsole> consoleModel=_appDbContext.tbl_console;
+            return View(consoleModel);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(MConsole consoleModel)
+        {
+            if (ModelState.IsValid) 
+            { 
+                _appDbContext.Add(consoleModel);
+                _appDbContext.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(consoleModel);
         }
         
     }
