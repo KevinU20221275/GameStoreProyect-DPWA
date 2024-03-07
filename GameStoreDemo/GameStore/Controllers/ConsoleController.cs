5449ffdd197d81c6a10a4392b9bea96ebdfbc650
KevinU20221275 <U20221275@univo.edu.sv>
2024-03-07 12:14:06 -0600

    the methods to Edit and Delete were created for the module console

--- GameStoreDemo/GameStore/Controllers/ConsoleController.cs
+++ GameStoreDemo/GameStore/Controllers/ConsoleController.cs
@@ -38,6 +38,66 @@ namespace GameStore.Controllers
             }
             return View(consoleModel);
         }
-        
+
+        [HttpGet]
+        public IActionResult Edit(int? id) 
+        {
+            if (id == null || id == 0) 
+            {
+                return NotFound();
+            }
+
+            var idConsole = _appDbContext.tbl_console.Find(id);
+
+            if (idConsole == null)
+            {
+                return NotFound();
+            }
+
+            return View(idConsole);
+        }
+
+
+        [HttpPost]
+        public IActionResult Edit(MConsole consoleModel)
+        {
+            if (ModelState.IsValid)
+            {
+                _appDbContext.Update(consoleModel);
+                _appDbContext.SaveChanges();
+                TempData["editConsole"] = "Se edito correctamente la Consola";
+                return RedirectToAction(nameof(Index));
+            }
+            return View(consoleModel);
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var idConsole = _appDbContext.tbl_console.Find(id);
+
+            if (idConsole == null)
+            {
+                return NotFound();
+            }
+
+            return View(idConsole);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(MConsole consoleModel)
+        {
+            
+            _appDbContext.Remove(consoleModel);
+            _appDbContext.SaveChanges();
+            TempData["deleteConsole"] = "Se Elimino correctamente la Consola";
+            return RedirectToAction(nameof(Index));
+            
+        }
     }
 }
