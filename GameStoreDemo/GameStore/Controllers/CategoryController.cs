04fc6a3aece916958392a2344d237d6dec4890d7
KevinU20221275 <U20221275@univo.edu.sv>
2024-03-14 12:01:15 -0600

    CRUD for finished Category

--- GameStoreDemo/GameStore/Controllers/CategoryController.cs
+++ GameStoreDemo/GameStore/Controllers/CategoryController.cs
@@ -1,13 +1,104 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using GameStore.Data;
+using GameStore.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace GameStore.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly AppDbContext _appDbContext;
+
+        public CategoryController(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+
         // GET: CategoryController
         public IActionResult Index()
         {
+            IEnumerable<MCategory> categoryModel = _appDbContext.tbl_category;
+            var objCategory = categoryModel.OrderBy(categoryModel=> categoryModel.Order).ToList();
+            return View(objCategory);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(MCategory categoryModel)
+        {
+            if (ModelState.IsValid)
+            {
+                _appDbContext.Add(categoryModel);
+                _appDbContext.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(categoryModel);
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var idCategory = _appDbContext.tbl_category.Find(id);
+
+            if (idCategory == null)
+            {
+                return NotFound();
+            }
+
+            return View(idCategory);
+        }
+
+
+        [HttpPost]
+        public IActionResult Edit(MCategory categoryModel)
+        {
+            if (ModelState.IsValid)
+            {
+                _appDbContext.Update(categoryModel);
+                _appDbContext.SaveChanges();
+                TempData["editCategory"] = "Se edito correctamente la Categoria";
+                return RedirectToAction(nameof(Index));
+            }
+            return View(categoryModel);
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var idCategory = _appDbContext.tbl_category.Find(id);
+
+            if (idCategory == null)
+            {
+                return NotFound();
+            }
+
+            return View(idCategory);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(MCategory categoryModel)
+        {
+
+            _appDbContext.Remove(categoryModel);
+            _appDbContext.SaveChanges();
+            TempData["deleteCategory"] = "Se Elimino correctamente la Categoria";
+            return RedirectToAction(nameof(Index));
+
         }
     }
 }
--- GameStoreDemo/GameStore/Controllers/ProductController.cs
+++ GameStoreDemo/GameStore/Controllers/ProductController.cs
@@ -1,13 +1,103 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using GameStore.Data;
+using GameStore.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace GameStore.Controllers
 {
     public class ProductController : Controller
     {
+        private readonly AppDbContext _appDbContext;
+
+        public ProductController(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+
         // GET: ProductController
-        public ActionResult Index()
+        public IActionResult Index()
+        {
+            IEnumerable<MProduct> productModel = _appDbContext.tbl_product;
+            return View(productModel);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(MProduct productModel)
+        {
+            if (ModelState.IsValid)
+            {
+                _appDbContext.Add(productModel);
+                _appDbContext.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(productModel);
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var idProduct = _appDbContext.tbl_product.Find(id);
+
+            if (idProduct == null)
+            {
+                return NotFound();
+            }
+
+            return View(idProduct);
+        }
+
+
+        [HttpPost]
+        public IActionResult Edit(MProduct productModel)
+        {
+            if (ModelState.IsValid)
+            {
+                _appDbContext.Update(productModel);
+                _appDbContext.SaveChanges();
+                TempData["editProduct"] = "Se edito correctamente el Producto";
+                return RedirectToAction(nameof(Index));
+            }
+            return View(productModel);
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var idProduct = _appDbContext.tbl_product.Find(id);
+
+            if (idProduct == null)
+            {
+                return NotFound();
+            }
+
+            return View(idProduct);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(MProduct productModel)
+        {
+
+            _appDbContext.Remove(productModel);
+            _appDbContext.SaveChanges();
+            TempData["deleteProduct"] = "Se Elimino correctamente el Producto";
+            return RedirectToAction(nameof(Index));
+
         }
 
     }
